0b3f71b66d991ee32b0f4ed6ccf07a72ec8cbf5f
IamFyrus <cyrusazad2442@gmail.com>
2025-02-11 18:55:49 -0800

    Update db.py

--- 165a-winter-2024-main/lstore/db.py
+++ 165a-winter-2024-main/lstore/db.py
@@ -20,21 +20,33 @@ class Database():
     :param key: int             #Index of table key in columns
     """
     def create_table(self, name, num_columns, key_index):
+        # Check if table name already exists
+        for table in self.tables:
+            if table.name == name:
+                return None  # Table name must be unique
+
         table = Table(name, num_columns, key_index)
-        self.tables[name] = table.name
+        self.tables.append(table)  # Append table to list
         return table
+
 
     
     """
     # Deletes the specified table
     """
     def drop_table(self, name):
-        if name in self.tables:
-            del self.tables[name]
+        for i, table in enumerate(self.tables):
+            if table.name == name:
+                del self.tables[i]  # Remove table from the list
+                return True
+        return False  # Table not found
 
     
     """
     # Returns table with the passed name
     """
     def get_table(self, name):
-        return self.tables.get(name, None)
+        for table in self.tables:
+            if table.name == name:
+                return table  # Return matching table
+        return None  # Table not found
