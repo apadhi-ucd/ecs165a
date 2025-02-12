85b7a74912b2e7fba88346ee8311ac9ad22ed763
Kenny5302 <166746503+Kenny5302@users.noreply.github.com>
2025-02-11 21:56:49 -0800

    Update query.py variables

--- 165a-winter-2024-main/lstore/query.py
+++ 165a-winter-2024-main/lstore/query.py
@@ -45,7 +45,7 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select(self, search_key, search_key_index, projected_columns_index):
-        record = self.table.select(search_key, search_key_index, projected_columns_index)
+        record = self.table.select_version(search_key, search_key_index, projected_columns_index)
         if record:
             return record
         else:
