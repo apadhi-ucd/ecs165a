58fb6c35111f9f0ca292a437e6541ee24b2042e1
kevin-newin <89609671+kevin-newin@users.noreply.github.com>
2025-02-13 19:22:18 -0800

    Update page.py

--- 165a-winter-2024-main/lstore/page.py
+++ 165a-winter-2024-main/lstore/page.py
@@ -17,11 +17,14 @@ class Page:
     :param value: int - Integer value to write
     """
     def write(self, value):
+        # Write the value to the page
         if not self.has_capacity():
             return False
         
+        # Calculate the offset
         offset = self.num_records * 8
         
+        # Write the value to the page
         value_bytes = value.to_bytes(8, byteorder='big', signed=True)
         self.data[offset:offset+8] = value_bytes
         self.num_records += 1
@@ -32,9 +35,13 @@ class Page:
     :param index: int - Index of the record to read
     """
     def read(self, index):
+        # Read the value from the page
         if index >= self.num_records:
             return None
             
+        # Calculate the offset
         offset = index * 8
+
+        # Read the value from the page
         value_bytes = self.data[offset:offset+8]
         return int.from_bytes(value_bytes, byteorder='big', signed=True)
