2f9b800a7b4331152a7533710c4408947af610e0
Kenny5302 <166746503+Kenny5302@users.noreply.github.com>
2025-02-26 06:28:44 -0800

    Page Kenny

--- 165a-winter-2024-main/lstore/page.py
+++ 165a-winter-2024-main/lstore/page.py
@@ -5,40 +5,46 @@ import zlib
 
 class Page:
     def __init__(self):
-        self.num_records = 0
-        self.data = bytearray(PAGE_SIZE)
-
+        self.entry_count = 0
+        self.content = bytearray(PAGE_SIZE)
+    
     def has_capacity(self):
-        return self.num_records < MAX_RECORD_PER_PAGE
-
+        """Check if page can store additional records"""
+        return self.entry_count < MAX_RECORD_PER_PAGE
+    
     def write(self, value):
-        struct.pack_into("i", self.data, self.num_records * INTEGER_BYTE_SIZE, value)
-        self.num_records += 1
-        return (self.num_records - 1)
-
-    def write_precise(self, index, value):
-        '''
-        This function should be able to write data on a precise index inside the table.
-        Useful for changing the indirection column on the base page
-        '''
-        struct.pack_into("i", self.data, index * INTEGER_BYTE_SIZE, value)
-
-    def get(self, index):
-        '''This funciton should be able to grab a data located at a certain index in the page'''
-        return struct.unpack_from("i", self.data, index * INTEGER_BYTE_SIZE)[0]
+        """Append a new integer value to the page and return its index"""
+        struct.pack_into("i", self.content, self.entry_count * INTEGER_BYTE_SIZE, value)
+        self.entry_count += 1
+        return (self.entry_count - 1)
+    
+    def write_precise(self, position, value):
+        """
+        Write an integer value at a specific position in the page.
+        Used for updating values like indirection pointers in base pages.
+        """
+        struct.pack_into("i", self.content, position * INTEGER_BYTE_SIZE, value)
+    
+    def get(self, position):
+        """Retrieve the integer value stored at the specified position"""
+        return struct.unpack_from("i", self.content, position * INTEGER_BYTE_SIZE)[0]
     
     def serialize(self):
-        '''Returns page metadata as a JSON-compatible dictionary'''
-        compressed_data = zlib.compress(self.data)
+        """
+        Convert page data to a serializable format.
+        Returns a dictionary with compressed page data in base64 encoding.
+        """
+        compressed_content = zlib.compress(self.content)
         return {
-            "num_records": self.num_records,
-
-            # converts bytearray into b64, which is then converted into a string (JSON compatible)
-            "data": base64.b64encode(compressed_data).decode('utf-8')
+            "entry_count": self.entry_count,
+            "content": base64.b64encode(compressed_content).decode('utf-8')
         }
-
-    def deserialize(self, json_data):
-        '''Loads a page from serialized data'''
-        self.num_records = json_data["num_records"]
-        compressed_data = base64.b64decode(json_data["data"])
-        self.data = bytearray(zlib.decompress(compressed_data))
+    
+    def deserialize(self, serialized_data):
+        """
+        Restore page from serialized data.
+        Unpacks the compressed base64-encoded content back into page.
+        """
+        self.entry_count = serialized_data["entry_count"]
+        compressed_content = base64.b64decode(serialized_data["content"])
+        self.content = bytearray(zlib.decompress(compressed_content))
